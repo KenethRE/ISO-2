8e6a77c2d362e83f788be4448fcd9463dbcca1ff
Allipson-ISO2 <114161839+Allipson-ISO2@users.noreply.github.com>
2022-10-17 21:16:28 +0200

    Update ProfesorExterno.java

--- GDTP/src/main/negocio/entities/ProfesorExterno.java
+++ GDTP/src/main/negocio/entities/ProfesorExterno.java
@@ -1,5 +1,20 @@
 package negocio.entities;
 
 public class ProfesorExterno extends Profesor {
+	
+	public ProfesorExterno(String dni, String nombre, String apellidos, boolean doctor) {
+		super(dni, nombre, apellidos, doctor);
+	}
+
 	private String _titulacion;
+
+	public String get_titulacion() {
+		return _titulacion;
+	}
+
+	public void set_titulacion(String _titulacion) {
+		this._titulacion = _titulacion;
+	}
+	
+	
 }
