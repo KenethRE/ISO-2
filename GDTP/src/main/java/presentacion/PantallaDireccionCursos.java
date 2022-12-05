ba21fb55a115c4d9a60229d1fb7a7f2679e21d11
david <arriero.david@gmail.com>
2022-12-05 13:37:02 +0100

    añadida pantalla direccion cursos

--- GDTP/src/main/java/presentacion/PantallaDireccionCursos.java
+++ GDTP/src/main/java/presentacion/PantallaDireccionCursos.java
@@ -1,6 +1,67 @@
 package presentacion;
 
-public class PantallaDireccionCursos {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout.Alignment;
+
+import java.awt.GridLayout;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class PantallaDireccionCursos extends JFrame{
+	JFrame previousWindow;
+	public PantallaDireccionCursos(JFrame previousWindow) {
+		this.previousWindow = previousWindow;
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		if (previousWindow != null) {
+			
+			this.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					previousWindow.setVisible(true);
+				}
+			});
+		}
+		setTitle("Pantalla Director Cursos");
+		setBounds(100, 100, 943, 610);
+		setResizable(false);
+		
+		JButton altaButton  = new JButton("Alta Curso");
+		altaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton edicionButton = new JButton("Editar curso");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addComponent(altaButton, GroupLayout.PREFERRED_SIZE, 464, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(edicionButton, GroupLayout.PREFERRED_SIZE, 459, GroupLayout.PREFERRED_SIZE)
+					.addGap(499))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(96)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(altaButton, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+						.addComponent(edicionButton, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(387, Short.MAX_VALUE))
+		);
+
+		getContentPane().setLayout(groupLayout);
+	}
+
+	
+	private static final long serialVersionUID = 1L;
 
 	public void altaCurso() {
 		throw new UnsupportedOperationException();
