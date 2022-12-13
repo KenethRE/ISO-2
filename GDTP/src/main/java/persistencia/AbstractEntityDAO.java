313e326c5c294b2edba8dd44424f8c05f5915e45
KenethRE <90388999+KenethRE@users.noreply.github.com>
2022-12-13 22:44:39 +0100

    Added changes for SQL updates with wrong syntax

--- GDTP/src/main/java/persistencia/AbstractEntityDAO.java
+++ GDTP/src/main/java/persistencia/AbstractEntityDAO.java
@@ -111,32 +111,34 @@ public class AbstractEntityDAO<E>{
 		switch (className) {
 		
 		case "CursoPropio":
-			result = agente.update("UPDATE INTO " + className + " VALUES('" + ((CursoPropio) E).get_id() + "','"  + ((CursoPropio) E).get_nombre() + "'," 
-					+ ((CursoPropio) E).get_eCTS() + ",'"  + ((CursoPropio) E).get_fechaInicio() + "','"  + ((CursoPropio) E).get_fechaFin() + "'," 
-					+ ((CursoPropio) E).get_tasaMatricula() + ","  + ((CursoPropio) E).get_edicion() + ","  + ((CursoPropio) E).getId_centro() + ",'"
-					+ ((CursoPropio) E).getId_secretario() + "','"  + ((CursoPropio) E).getId_director() + "')" + " WHERE id = '" + ((CursoPropio) E).get_id() +"'");
+			result = agente.update("UPDATE " + className + " SET ID='" + ((CursoPropio) E).get_id() + "', NOMBRE='"  + ((CursoPropio) E).get_nombre() + "', ECTS=" 
+					+ ((CursoPropio) E).get_eCTS() + ", FECHAINICIO='"  + ((CursoPropio) E).get_fechaInicio() + "', FECHAFIN='"  + ((CursoPropio) E).get_fechaFin() + "', TASAMATRICULA=" 
+					+ ((CursoPropio) E).get_tasaMatricula() + ", EDICION="  + ((CursoPropio) E).get_edicion() + ", IDCENTRO="  + ((CursoPropio) E).getId_centro() + ", SECRETARIO='"
+					+ ((CursoPropio) E).getId_secretario() + "', DIRECTOR='"  + ((CursoPropio) E).getId_director() + "', TIPOCURSO='" + ((CursoPropio) E).get_tipo() 
+					+ "', ESTADOCURSO='" + ((CursoPropio) E).get_estado() + "' WHERE ID = '" + ((CursoPropio) E).get_id() +"'");
 			break;
 		case "Profesor":
-			result = agente.update("UPDATE INTO " + className + " VALUES('" + ((Profesor) E).get_dni() + "','" + ((Profesor) E).get_nombre() + "','"
-					 + ((Profesor) E).get_apellidos() + "'," + ((Profesor) E).is_doctor()+ "," + ((Profesor) E).get_centro() + ")" + " WHERE dni = '" + ((Profesor) E).get_dni() +"'");
+			result = agente.update("UPDATE " + className + " SET DNI='" + ((Profesor) E).get_dni() + "', NOMBRE='" + ((Profesor) E).get_nombre() + "', APELLIDOS='"
+					 + ((Profesor) E).get_apellidos() + "', DOCTOR=" + ((Profesor) E).is_doctor()+ ", CENTRO=" + ((Profesor) E).get_centro() + ")" + " WHERE DNI = '" + ((Profesor) E).get_dni() +"'");
 		case "ProfesorExterno":
-			result = agente.update("UPDATE INTO " + className + "VALUES('" + ((ProfesorExterno) E).get_dni() + "','" + ((ProfesorExterno) E).get_titulacion() + "')"+ " WHERE dni = '" + ((ProfesorExterno) E).get_dni() +"'");
+			result = agente.update("UPDATE " + className + "SET DNI='" + ((ProfesorExterno) E).get_dni() + "', TITULACION='" + ((ProfesorExterno) E).get_titulacion() + "'"+ " WHERE DNI = '" + ((ProfesorExterno) E).get_dni() +"'");
 			break;
 		
 		case "Matricula":
-			result = agente.update("UPDATE INTO " + className + "VALUES('" + ((Matricula) E).get_fecha() + "'," + ((Matricula) E).is_pagado() + ",'"
-					+ ((Matricula) E).getId_estudiante() + "','" + ((Matricula) E).getId_Curso() + "')" + " WHERE ID = '" + ((Matricula) E).getiD() + "'");
+			result = agente.update("UPDATE " + className + "SET FECHA='" + ((Matricula) E).get_fecha() + "', PAGADO=" + ((Matricula) E).is_pagado() + ", DNIESTUDIANTE='"
+					+ ((Matricula) E).getId_estudiante() + "', IDCURSO='" + ((Matricula) E).getId_Curso() + "', MODOPAGO='" + ((Matricula) E).get_tipoPago()+ "' WHERE ID = '" + ((Matricula) E).getiD() + "'");
 		case "Materia":
-			result = agente.update("UPDATE INTO " + className + "VALUES('" + ((Materia) E).get_nombre()+ "'," + ((Materia) E).get_horas() + ",'"
-					+ ((Materia) E).get_fechaInicio() + "','" + ((Materia) E).get_fechaFin() + "','" + ((Materia) E).getId_Curso() + "')"+ " WHERE Nombre = '" + ((Materia) E).get_nombre() +"'"); 
+			result = agente.update("UPDATE " + className + "SET NOMBRE='" + ((Materia) E).get_nombre()+ "', HORAS=" + ((Materia) E).get_horas() + ", FECHAINICIO='"
+					+ ((Materia) E).get_fechaInicio() + "', FECHAFIN='" + ((Materia) E).get_fechaFin() + "', IDCURSO='" + ((Materia) E).getId_Curso() + "', DNIPROFESOR='" + ((Materia) E).getId_prof_responsable()+ "' WHERE NOMBRE = '" 
+					+ ((Materia) E).get_nombre() +"'"); 
 			break;
 		case "Centro":
-			result = agente.update("UPDATE INTO " + className + "VALUES(" + ((Centro) E).get_Id() + ",'" + ((Centro) E).get_nombre() + "','" + ((Centro) E).get_localizacion() 
-					+ "')"+ " WHERE id = '" + ((Centro) E).get_Id() +"'");
+			result = agente.update("UPDATE " + className + "SET ID=" + ((Centro) E).get_Id() + ", NOMBRE='" + ((Centro) E).get_nombre() + "', LOCALIZACION='" + ((Centro) E).get_localizacion() 
+					+ "' WHERE ID = '" + ((Centro) E).get_Id() +"'");
 			break;
 		case "Estudiante":
-			result = agente.update("UPDATE INTO " + className + "VALUES('" + ((Estudiante) E).get_dni() + "','" + ((Estudiante) E).get_nombre() + "','" + ((Estudiante) E).get_apellidos() 
-					+ "','" + ((Estudiante) E).get_titulacion() + "','" + ((Estudiante) E).get_cualificacion() + "')"+ " WHERE dni = '" + ((Estudiante) E).get_dni() +"'" );
+			result = agente.update("UPDATE " + className + "SET DNI='" + ((Estudiante) E).get_dni() + "', NOMBRE='" + ((Estudiante) E).get_nombre() + "', APELLIDOS='" + ((Estudiante) E).get_apellidos() 
+					+ "', TITULACION='" + ((Estudiante) E).get_titulacion() + "', CUALIFICACION='" + ((Estudiante) E).get_cualificacion() + "' WHERE DNI = '" + ((Estudiante) E).get_dni() +"'" );
 			break;
 		}	
 		return result;
