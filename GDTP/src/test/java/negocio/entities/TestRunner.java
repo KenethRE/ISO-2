61f7ab8d441bbe518970533aeb00b42021a9c2cd
KenethRE <90388999+KenethRE@users.noreply.github.com>
2022-12-13 22:25:27 +0100

    updated class

--- GDTP/src/test/java/negocio/entities/TestRunner.java
+++ GDTP/src/test/java/negocio/entities/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(MateriaTest.class); //cambias aqui el nombre de la clase para saber el resultado
+      Result result = JUnitCore.runClasses(CentroTest.class); //cambias aqui el nombre de la clase para saber el resultado
 		
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
