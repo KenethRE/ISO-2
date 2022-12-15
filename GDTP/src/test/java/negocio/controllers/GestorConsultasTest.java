1b5a628d00410371c0fea0b5fda976ca474e81c7
david <arriero.david@gmail.com>
2022-12-15 16:55:21 +0100

    gestor consultas testing

--- GDTP/src/test/java/negocio/controllers/GestorConsultasTest.java
+++ GDTP/src/test/java/negocio/controllers/GestorConsultasTest.java
@@ -1,44 +1,83 @@
+/**
+ * 
+ */
 package negocio.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import java.sql.Date;
 
-class GestorConsultasTest {
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+import negocio.entities.TipoCurso;
+
+/**
+ * @author plati
+ *
+ */
+public class GestorConsultasTest {
+	GestorConsultas gestorConsultas;
+	Date fechacomienzoDate;
+	Date fechafinDate;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		fechacomienzoDate= new java.sql.Date(System.currentTimeMillis()- 259200000);
+		fechafinDate= new java.sql.Date(System.currentTimeMillis());
+		gestorConsultas= new GestorConsultas();
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Test method for {@link negocio.controllers.GestorConsultas#consultarIngresos(negocio.entities.TipoCurso, java.sql.Date, java.sql.Date)}.
+	 */
 	@Test
-	void testConsultarIngresos() {
-		fail("Not yet implemented");
+	public void testConsultarIngresos() {
+		assertTrue("la lista devuelta por el metodo consultar ingreso esta vacia",gestorConsultas.consultarIngresos(TipoCurso.MASTER,fechacomienzoDate, fechafinDate).size()>0);
 	}
 
+	/**
+	 * Test method for {@link negocio.controllers.GestorConsultas#consultarEstadoCursos(negocio.entities.EstadoCurso, java.sql.Date, java.sql.Date)}.
+	 */
 	@Test
-	void testConsultarEstadoCursos() {
-		fail("Not yet implemented");
+	public void testConsultarEstadoCursos() {
+		//fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link negocio.controllers.GestorConsultas#listarEdicionesCursos(java.sql.Date, java.sql.Date)}.
+	 */
 	@Test
-	void testListarEdicionesCursos() {
-		fail("Not yet implemented");
+	public void testListarEdicionesCursos() {
+		//fail("Not yet implemented");
 	}
 
 }
