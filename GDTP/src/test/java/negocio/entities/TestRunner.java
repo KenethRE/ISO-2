358471102b2f3f12d8c4a4853b8aea8bb6f3b73e
Mrbanns <Mrbanns@192.168.1.10>
2022-12-20 23:04:03 +0100

    Test Curso Propio

--- GDTP/src/test/java/negocio/entities/TestRunner.java
+++ GDTP/src/test/java/negocio/entities/TestRunner.java
@@ -5,8 +5,59 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(CentroTest.class); //cambias aqui el nombre de la clase para saber el resultado
+	   
+      Result result = JUnitCore.runClasses(CentroTest.class); 
+      
+      System.out.println("Test Centro:");
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
 		
+      System.out.println(result.wasSuccessful());
+      
+      result = JUnitCore.runClasses(CursoPropioTest.class);
+     
+      System.out.println("Test CursoPropio:");
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+      result = JUnitCore.runClasses(MateriaTest.class);
+	
+      System.out.println("Test Materia:");
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+      result = JUnitCore.runClasses(MatriculaTest.class);
+		
+      System.out.println("Test Matricula:");
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+      result = JUnitCore.runClasses(ProfesorExternoTest.class);
+	
+      System.out.println("Test ProfesorExterno:");
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+      result = JUnitCore.runClasses(ProfesorTest.class);
+		
+      System.out.println("Test Profesor:");
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+      result = JUnitCore.runClasses(ProfesorUCLMTest.class);
+	
+      System.out.println("Test ProfesorUCLM:");
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
       }
